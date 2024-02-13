39e0b928bd70bd1275e9901049cf054ee6a1b339
amitfrid7 <fridamit@gmail.com>
2024-02-13 13:54:45 +0200

    Implementing ball increment

--- js/main.js
+++ js/main.js
@@ -2,8 +2,10 @@ var gBallSize = 100
 
 function onBallClick(ball) {
     gBallSize = gBallSize + 50
+    
+    if (gBallSize === 400) gBallSize = 100
     ball.style.width = gBallSize + 'px'
     ball.style.height = gBallSize + 'px'
-
+    
     ball.innerText = gBallSize
 }
