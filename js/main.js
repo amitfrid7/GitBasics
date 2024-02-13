362aae64cfe9359a9304715c8d0f2088e733ab64
amitfrid7 <fridamit@gmail.com>
2024-02-13 15:47:58 +0200

    Add 3rd ball and implementing color+size swap function

--- js/main.js
+++ js/main.js
@@ -10,3 +10,28 @@ function onBallClick(ball, maxDiameter) {
     ball.style.backgroundColor = getRandomColor()
     ball.innerText = gBallSize
 }
+
+function onBall3Click() {
+
+    var ball1 = document.querySelector('.ball1')
+    var ball2 = document.querySelector('.ball2')
+
+    var holder = {  
+                width: ball1.style.width,
+                height: ball1.style.height, 
+                color: ball1.style.backgroundColor,
+                size: ball1.innerText
+            }
+
+    ball1.style.width = ball2.style.width
+    ball1.style.height = ball2.style.height
+    ball1.style.backgroundColor = ball2.style.backgroundColor
+    ball1.innerText = ball2.innerText
+
+    
+    ball2.style.width = holder.width
+    ball2.style.height = holder.height
+    ball2.style.backgroundColor = holder.color
+    ball2.innerText = holder.size
+
+}
