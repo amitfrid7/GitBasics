3a263e37e2f78722eff2a0a16f899590f95cd687
amitfrid7 <fridamit@gmail.com>
2024-02-13 15:14:37 +0200

    Adding random size and color to the ball

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 var gBallSize = 100
 
 function onBallClick(ball) {
-    gBallSize = gBallSize + 50
+    gBallSize = gBallSize + getRandomInt(20, 60)
     
-    if (gBallSize === 400) gBallSize = 100
+    if (gBallSize >= 400) gBallSize = 100
     ball.style.width = gBallSize + 'px'
     ball.style.height = gBallSize + 'px'
     
+    ball.style.backgroundColor = getRandomColor()
     ball.innerText = gBallSize
 }
