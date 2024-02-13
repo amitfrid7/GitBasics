965ff0200bb901ea41c9d985ee384b6cd7ecdaae
amitfrid7 <fridamit@gmail.com>
2024-02-13 13:40:25 +0200

    Implement onBallClick and style ball

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
-function onBallClick() {
-    console.log('Hi')
+var gBallSize = 100
+
+function onBallClick(ball) {
+    gBallSize = gBallSize + 50
+    ball.style.width = gBallSize + 'px'
+    ball.style.height = gBallSize + 'px'
+
+    ball.innerText = gBallSize
 }
